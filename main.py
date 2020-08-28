f21b2dfb7a008121ad396d089bbc4368df6028df
aadi350 <a.aadi380@gmail.com>
2020-08-28 17:50:39 -0400

    Change to text prompt for credentials Print course info

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
+print('Enter ID')
+username = input()
+print('Enter password')
+pwd = input()
 
-browser = webdriver.Chrome('C:\driver\chromedriver.exe')
+options = Options()
+options.binary_location = 'D:\Program Files\Google\Chrome\Application\chrome.exe'
+browser = webdriver.Chrome(executable_path=r'D:\chromedriver\chromedriver.exe', options=options)
 browser.implicitly_wait(2)
 
 browser.get("https://mycas.sta.uwi.edu/cas/login?service=https%3A%2F%2Fssb2.sta.uwi.edu%2Fproddad%2Ftwzkcasl.P_Service_Ticket%3Ftarget%3Dbwsksmrk.p_write_term_selection")
@@ -12,8 +19,10 @@ browser.get("https://mycas.sta.uwi.edu/cas/login?service=https%3A%2F%2Fssb2.sta.
 username_input = browser.find_element_by_id('username')
 pwd_input = browser.find_element_by_id('password')
 
-username_input.send_keys('816003022')
-pwd_input.send_keys('K5D5#4c3')
+
+
+username_input.send_keys(username)
+pwd_input.send_keys(pwd)
 
 login_button = browser.find_element_by_name('submit')
 login_button.click()
@@ -23,5 +32,11 @@ submit_term_button.click()
 
 course_code_link = browser.find_element_by_xpath("//*[contains(text(), '28457')]")
 course_code_link.click()
+
+course_info = browser.find_elements_by_class_name('ntdefault')
+course_info_labels = browser.find_elements_by_class_name('ntlabel')
+
+for info, labels in zip(course_info, course_info_labels):
+    print('{} {}'.format(labels.text, info.text))
 
 sleep(5)
